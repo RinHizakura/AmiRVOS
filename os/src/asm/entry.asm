6c47657eec3cf88ff4e4b1b6c0f745cbba83c0ac
RinHizakura <s921975628@gmail.com>
2023-10-06 23:05:59 +0800

    Update the initial assembly for latest qemu

--- os/src/asm/entry.asm
+++ os/src/asm/entry.asm
@@ -1,84 +1,94 @@
 .section .text.boot
 .global _start
 _start:
-	# For the hart that id != 0, hanging in a infinite for loop
-	csrr	t0, mhartid
-	bnez	t0, 3f
+    # For the hart that id != 0, hanging in a infinite for loop
+    csrr    t0, mhartid
+    bnez    t0, 3f
 
-	csrw	satp, zero
+    csrw    satp, zero
 
-	# .bss section is reset to be zero
-	la 	a0, _bss_start
-	la	a1, _bss_end
-	bgeu	a0, a1, 2f
+    # .bss section is reset to be zero
+    la      a0, _bss_start
+    la      a1, _bss_end
+    bgeu    a0, a1, 2f
 1:
-	sd	zero, (a0)
-	addi	a0, a0, 8
-	bltu	a0, a1, 1b
+    sd      zero, (a0)
+    addi    a0, a0, 8
+    bltu    a0, a1, 1b
 2:
-	la	sp, _stack_end
+    la      sp, _stack_end
 
-        # (0b11 << 11): the privilege level will be set to 3(machine mode) after mret
-        # for bit 7 of mstatus, the machine mode interrupt-enable bit will be 0 after mret
-        li	t0, (0b11 << 11)
-	csrw	mstatus, t0
+    # (0b11 << 11): the privilege level will be set to 3(machine mode) after mret
+    # for bit 7 of mstatus, the machine mode interrupt-enable bit will be 0 after mret
+    li      t0, (0b11 << 11)
+    csrw    mstatus, t0
 
-        # pc will be set to address of kinit after mret
-	la	t1, kinit
-	csrw	mepc, t1
+    # pc will be set to address of kinit after mret
+    la      t1, kinit
+    csrw    mepc, t1
 
-	la	ra, 2f
-	mret
+    la      ra, 2f
+    mret
 2:
-        # (1 << 11): the privilege level will be set to 1(supervisor mode) after mret
-        # (1 << 7): the machine mode interrupt-enable bit will be 1 after mret
-        # (1 << 5): the supervisor mode interrupt-enable bit will be 1 after mret
-        li	t0, (1 << 11) | (1 << 7) | (1 << 5)
-        csrw	mstatus, t0
+    # (1 << 11): the privilege level will be set to 1(supervisor mode) after mret
+    # (1 << 7): the machine mode interrupt-enable bit will be 1 after mret
+    # (1 << 5): the supervisor mode interrupt-enable bit will be 1 after mret
+    li      t0, (1 << 11) | (1 << 7) | (1 << 5)
+    csrw    mstatus, t0
 
-        # pc will be set to address of kmain after sret
-        # note that MMU will be switched on after sret, so we have to
-        # set sepc to the virtual address of kmain
-	la	t0, kmain
-        csrw	mepc, t0
+    # M-mode and S-mode software interrupt, timer interrupt, and external
+    # interrupt are enabled
+    li      t0, 0xa | (0xa << 4) | (0xa << 8)
+    csrw    mie, t0
 
-        # M-mode and S-mode software interrupt, timer interrupt, and external
-        # interrupt are enabled
-	li	t0, 0xa | (0xa << 4) | (0xa << 8)
-	csrw	mie, t0
+    # delegate software interrupt, timer interrupt, and external interrupt from
+    # M-mode to S-mode
+    li      t2, 0xffff
+    csrw    mideleg, t2
 
-        # delegate software interrupt, timer interrupt, and external interrupt from
-        # M-mode to S-mode
-        li	t2, 0xffff
-	csrw	mideleg, t2
+    # delegate ebreak from M-mode to S-mode
+    li      t2, (1 << 3)
+    csrw    medeleg, t2
 
-        # delegate ebreak from M-mode to S-mode
-        li	t2, (1 << 3)
-	csrw	medeleg, t2
+    # set up page table and corresponding mode for virtual addressing
+    la      t0, boot_page_table
+    srli    t0, t0, 12
+    li      t1, (8 << 60)
+    or      t0, t0, t1
+    csrw    satp, t0
+    sfence.vma
 
-        # set up page table and corresponding mode for virtual addressing
-        la	t0, boot_page_table
-        srli	t0, t0, 12
-        li	t1, (8 << 60)
-        or	t0, t0, t1
-        csrw	satp, t0
-        sfence.vma
+    # mtvec is only store to respond if wrong irq handler is triggered
+    la      t0, m_trap_vector
+    csrw    mtvec, t0
 
-        # mtvec is only store to respond if wrong irq handler is triggered
-        la	t0, m_trap_vector
-	csrw	mtvec, t0
+    # In AmiRVOS, we'll prefer to delegate trap to Supervisor mode, so the
+    # expected entry point of handler should be in stvec
+    la      t0, s_trap_vector
+    csrw    stvec, t0
 
-        # In AmiRVOS, we'll prefer to delegate trap to Supervisor mode, so the
-        # expected entry point of handler should be in stvec
-	la	t0, s_trap_vector
-	csrw	stvec, t0
+    # pc will be set to address of kmain after sret
+    # note that MMU will be switched on after sret, so we have to
+    # set sepc to the virtual address of kmain
+    la      t0, kmain
+    csrw    mepc, t0
 
-        # hanging if return from kmain
-	la	ra, 3f
-        mret
+    # We need to set the PMP entry to start kmain correctly. To be simple at
+    # first, we just use single pmp with unrestricted privilege to cover whole
+    # memory region. See https://stackoverflow.com/questions/69133848/risc-v-illegal-instruction-exception-when-switching-to-supervisor-mode
+    li      t0, 0xffffffffffffffff
+    csrw    pmpaddr0, t0
+    # (0b11 << 3): NAPOT
+    # 0x7: read, write, execute are valid
+    li      t0, (0b11 << 3) | 0x7
+    csrw    pmpcfg0, t0
+
+    # hanging if return from kmain
+    la      ra, 3f
+    mret
 3:
-	wfi
-	j	3b
+    wfi
+    j       3b
 
 # Initialize a page table to map the kernel in booting stage. We use 1G resolution pages
 # to simplify the mapping work with assembly first, and change to 4K resolution pages
