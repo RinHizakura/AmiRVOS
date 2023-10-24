04bb8e6c7f2083d8011681dd9cad52e0c2e881bf
Yiwei Lin <s921975628@gmail.com>
2023-10-24 21:33:57 +0800

    Switch task faster

--- os/src/clint.rs
+++ os/src/clint.rs
@@ -1,6 +1,9 @@
 use riscv::register::{sie, sstatus};
 
-const INTERVAL: usize = 10_000_000;
+/* The frequency given by QEMU is 10_000_000 Hz, thus
+ * the interval 2_500_000 means that we tick every 250ms
+ * to switch our task. */
+const INTERVAL: usize = 2_500_000;
 
 mmap_reg!(mtimecmp, 0x200_0000 + 0x4000, usize);
 mmap_reg!(mtime, 0x200_0000 + 0xbff8, usize);
